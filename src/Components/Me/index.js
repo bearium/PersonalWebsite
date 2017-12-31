9a054ae1ca7d8e82275cda85794114ccb56030a9
Jacob <jacob_martin@rogers.com>
2017-12-30 23:51:54 -0500

    started working on individual components

--- src/Components/Me/index.js
+++ src/Components/Me/index.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import "./me.css"
-
+import pic from "./me.jpg"
 
 class Me extends Component {
     render() {
         return (
-            <div>these are my projects</div>
+
+            <div>
+                <img className="me" src={pic} alt="avatar"></img>
+
+                <p>this is adsd me</p>
+
+
+            </div>
         );
     }
 }
