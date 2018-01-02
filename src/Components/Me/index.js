1442ccc1af960ce9606b425b4cae5534750631f9
Jacob <jacob_martin@rogers.com>
2018-01-02 01:36:20 -0500

    added more styling and descriptions, as well started on projects page.

--- src/Components/Me/index.js
+++ src/Components/Me/index.js
@@ -11,8 +11,12 @@ class Me extends Component {
                 <img className="me" src={pic} alt="avatar"/>
 
                 <h1 className="Title">Jacob Martin</h1>
-                <p className="Title">Computer Systems Engineering Student sample</p>
-
+                <p className="Title">Computer Systems Engineering Student</p>
+                <p className= "Title"> Hello, my name is Jacob. I am a 3rd year computer systems engineering student at Carleton University. I am a highly motivated
+                    student. I have a thorough understanding of data structures and algorithms, and web development. I enjoy being challenged and seeking opportunities
+                    to expand my knowledge and show off my skills. I have experience in multiple software languages. I have participated in multiple hackathons across Ottawa, my team and I
+                    won several of these hackathons with our programs. Please explore the site to see a few of our designs.
+                    This website was designed and coded entirely by me. I hope this site gives you a good understanding of my abilities and knowledge. </p>
 
             </div>
         );
--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,11 +1,99 @@
 import React, { Component } from 'react';
 import "./projects.css"
-
+import ryke from "./ryke.png"
+import xmas from "./xmas.jpg"
+import resume from "./resume.png"
+import lunar from "./lunar.png"
 
 class Projects extends Component {
     render() {
         return (
-            <div>these are my projects</div>
+            <div className="fade">
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <div className="column1">
+                                <div className="box">
+                                    <div className="content">
+                                        <div className='image fit'>
+                                          <img src = {ryke} alt/>
+                                        </div>
+                                        <header className="align-center">
+                                            <p>Software name</p>
+                                            <h2>Website</h2>
+                                        </header>
+                                        <p className="align-center"> description.</p>
+                                        <footer className="align-center">
+                                            <a href="#" className="button alt">visit</a>
+                                        </footer>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <div className="box">
+                                    <div className="content">
+                                        <div className='image fit'>
+                                            <img src = {xmas} alt/>
+                                        </div>
+                                        <header className="align-center">
+                                            <p>Software name</p>
+                                            <h2>Website</h2>
+                                        </header>
+                                        <p className="align-center"> description.</p>
+                                        <footer className="align-center">
+                                            <a href="#" className="button alt">visit</a>
+                                        </footer>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                    <td>
+                        <div className="column1">
+                            <div className="box">
+                                <div className="content">
+                                    <div className='image fit'>
+                                        <img src = {resume} alt/>
+                                    </div>
+                                    <header className="align-center">
+                                        <p>Software name</p>
+                                        <h2>Website</h2>
+                                    </header>
+                                    <p className="align-center"> description.</p>
+                                    <footer className="align-center">
+                                        <a href="#" className="button alt">visit</a>
+                                    </footer>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <div className="column1">
+                            <div className="box">
+                                <div className="content">
+                                    <div className='image fit'>
+                                        <img src = {lunar} alt/>
+                                    </div>
+                                    <header className="align-center">
+                                        <p>Software name</p>
+                                        <h2>Website</h2>
+                                    </header>
+                                    <p className="align-center"> description.</p>
+                                    <footer className="align-center">
+                                        <a href="#" className="button alt">visit</a>
+                                    </footer>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }
