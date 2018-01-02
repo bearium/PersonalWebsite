f9a5b69e8d375c8fbd120e63ad2bc4a067bbeab2
Jacob <jacob_martin@rogers.com>
2018-01-01 20:57:13 -0500

    added transitions and changed photo

--- src/Components/Me/index.js
+++ src/Components/Me/index.js
@@ -7,11 +7,11 @@ class Me extends Component {
         return (
 
             <div className="page1">
-                <br></br>
-                <img className="me" src={pic} alt="avatar"></img>
+                <br/>
+                <img className="me" src={pic} alt="avatar"/>
 
                 <h1 className="Title">Jacob Martin</h1>
-                <p className="Title">Computer Systems Engineering Student</p>
+                <p className="Title">Computer Systems Engineering Student sample</p>
 
 
             </div>
